122bbd2d2cda24a64092ec5bf289aa844699275f
Ajshawit <ajshawit@gmail.com>
2016-12-20 18:12:39 +1100

    Adding date update using moment.js

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,47 @@ function Slider() {
 $(function() {
   setInterval(Slider, 20000);
 });
+
+// Update Date
+
+$('#day').text(moment().format('DD'));
+$('#month').text(moment().format('MMMM'));
+
+
+// Weather Plugin
+
+var fullDatePlus0 = moment().format('YYYY-MM-DD');
+var fullDatePlus1 = moment().add(1, 'days').format('YYYY-MM-DD');
+var fullDatePlus2 = moment().add(2, 'days').format('YYYY-MM-DD');
+var fullDatePlus3 = moment().add(3, 'days').format('YYYY-MM-DD');
+var fullDatePlus4 = moment().add(4, 'days').format('YYYY-MM-DD');
+
+$.getJSON('http://api.openweathermap.org/data/2.5/forecast?id=6619279&appid=cb00cfb85549a09b384addf89d365ed6&mode=json&units=metric', function(json) {
+  for (i = 0; i < json.cnt; i++) {
+    if (moment.unix(json.list[i].dt).format('YYYY-MM-DD h:mm:ss a').includes(fullDatePlus0)) {
+      $('.weather0').text(json.list[0].weather[0].main);
+      $('.temp0').text(json.list[0].main.temp);
+      $('.date0').text(moment.unix(json.list[0].dt).format('dddd'));
+    }
+    if (moment.unix(json.list[i].dt).format('YYYY-MM-DD h:mm:ss a').includes(fullDatePlus1)) {
+      $('.weather1').text(json.list[i].weather[0].main);
+      $('.temp1').text(json.list[i].main.temp);
+      $('.date1').text(moment.unix(json.list[i].dt).format('dddd'));
+    }
+    if (moment.unix(json.list[i].dt).format('YYYY-MM-DD h:mm:ss a').includes(fullDatePlus2)) {
+      $('.weather2').text(json.list[i].weather[0].main);
+      $('.temp2').text(json.list[i].main.temp);
+      $('.date2').text(moment.unix(json.list[i].dt).format('dddd'));
+    }
+    if (moment.unix(json.list[i].dt).format('YYYY-MM-DD h:mm:ss a').includes(fullDatePlus3)) {
+      $('.weather3').text(json.list[i].weather[0].main);
+      $('.temp3').text(json.list[i].main.temp);
+      $('.date3').text(moment.unix(json.list[i].dt).format('dddd'));
+    }
+    if (moment.unix(json.list[i].dt).format('YYYY-MM-DD h:mm:ss a').includes(fullDatePlus4)) {
+      $('.weather4').text(json.list[i].weather[0].main);
+      $('.temp4').text(json.list[i].main.temp);
+      $('.date4').text(moment.unix(json.list[i].dt).format('dddd'));
+    }
+  }
+});
